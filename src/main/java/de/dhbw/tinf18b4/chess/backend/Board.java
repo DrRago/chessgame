ac4f88d86f81dbeec698d54df8dd3f78e2886baa
Stefan Lukas <afc11hn@hotmail.de>
2019-06-28 17:58:18 +0200

    Add a stub implementation for Board

---
+++ src/main/java/de/dhbw/tinf18b4/chess/backend/Board.java
@@ -0,0 +1,26 @@
+package de.dhbw.tinf18b4.chess.backend;
+
+class Board {
+    /**
+     * Check whether a move is allowed on this board
+     * <p>
+     * TODO: Implement
+     *
+     * @param move The move
+     * @return whether is possible to make the move
+     */
+    boolean checkMove(Game.Move move) {
+        return false;
+    }
+
+    /**
+     * Apply a move to the board
+     * <p>
+     * TODO: Implement
+     *
+     * @param move The move
+     */
+    void applyMove(Game.Move move) {
+
+    }
+}
