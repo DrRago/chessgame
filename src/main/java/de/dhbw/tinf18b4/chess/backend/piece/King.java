5158a0020a3557696531054595f63345e2ee37bc
Stefan Lukas <afc11hn@hotmail.de>
2019-07-01 22:57:28 +0200

    Remove duplicate javadoc

--- src/main/java/de/dhbw/tinf18b4/chess/backend/piece/King.java
+++ src/main/java/de/dhbw/tinf18b4/chess/backend/piece/King.java
@@ -19,34 +19,17 @@ public class King implements Piece {
         this.position = position;
     }
 
-    /**
-     * Move the piece to the given position. Returns true if the move was valid, false if it wasn't
-     *
-     * @param position the destination position
-     * @return whether the move was made or not
-     */
     @Override
     public boolean moveTo(Position position) {
         this.position = position;
         return true;
     }
 
-    /**
-     * Get the position of the piece on the board
-     *
-     * @return where the piece is
-     */
     @Override
     public Position getPosition() {
         return position;
     }
 
-    /**
-     * Get a list of all possible moves for the piece. Dies not include kill moves
-     *
-     * @return a list of all possible moves
-     * @param board
-     */
     @Override
     public List<Position> getValidMoves(Board board) {
         // TODO: 01.07.2019 Implement castling special move
@@ -59,32 +42,16 @@ public class King implements Piece {
                 .collect(Collectors.toList());
     }
 
-    /**
-     * Get a list of all possible capture options
-     *
-     * @return all capture options
-     * @param board
-     */
     @Override
     public List<Position> getValidCaptureMoves(Board board) {
         return getValidMoves(board);
     }
 
-    /**
-     * Get the color of the piece
-     *
-     * @return whether the piece is white or not
-     */
     @Override
     public boolean isWhite() {
         return white;
     }
 
-    /**
-     * Get the capture state of the piece
-     *
-     * @return whether the piece has been captured or not
-     */
     @Override
     public boolean isCaptured() {
         return false;
--- src/main/java/de/dhbw/tinf18b4/chess/backend/piece/Knight.java
+++ src/main/java/de/dhbw/tinf18b4/chess/backend/piece/Knight.java
@@ -20,34 +20,17 @@ public class Knight implements Piece {
         this.position = position;
     }
 
-    /**
-     * Move the piece to the given position. Returns true if the move was valid, false if it wasn't
-     *
-     * @param position the destination position
-     * @return whether the move was made or not
-     */
     @Override
     public boolean moveTo(Position position) {
         this.position = position;
         return true;
     }
 
-    /**
-     * Get the position of the piece on the board
-     *
-     * @return where the piece is
-     */
     @Override
     public Position getPosition() {
         return position;
     }
 
-    /**
-     * Get a list of all possible moves for the piece. Dies not include kill moves
-     *
-     * @return a list of all possible moves
-     * @param board
-     */
     @Override
     public List<Position> getValidMoves(Board board) {
         // Knights always move 3 squares. First they 2 vertical or horizontal and then they make a turn.
@@ -92,32 +75,16 @@ public class Knight implements Piece {
                 .collect(Collectors.toList());
     }
 
-    /**
-     * Get a list of all possible capture options
-     *
-     * @return all capture options
-     * @param board
-     */
     @Override
     public List<Position> getValidCaptureMoves(Board board) {
         return getValidMoves(board);
     }
 
-    /**
-     * Get the color of the piece
-     *
-     * @return whether the piece is white or not
-     */
     @Override
     public boolean isWhite() {
         return white;
     }
 
-    /**
-     * Get the capture state of the piece
-     *
-     * @return whether the piece has been captured or not
-     */
     @Override
     public boolean isCaptured() {
         return false;
