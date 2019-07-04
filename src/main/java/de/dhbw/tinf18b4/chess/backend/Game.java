751fd8aa6e23220e2c562311247a647182971b8e
Stefan Lukas <afc11hn@hotmail.de>
2019-07-04 13:11:26 +0200

    Remove some unused preparation for FEN serialization

--- src/main/java/de/dhbw/tinf18b4/chess/backend/Game.java
+++ src/main/java/de/dhbw/tinf18b4/chess/backend/Game.java
@@ -19,9 +19,6 @@ public class Game {
     private Player player1;
     private Player player2;
 
-    private int halfMoveClock = 0;
-    private int fullMoves = 1;
-
     public Game(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
@@ -58,22 +55,10 @@ public class Game {
 
             history.push(move);
 
-            if (!move.getPlayer().isWhite()) {
-                fullMoves++;
-            }
-
             return true;
         }
 
         return false;
-    }
-
-    public int getFullMoves() {
-        return fullMoves;
-    }
-
-    public int getHalfMoveClock() {
-        return halfMoveClock;
     }
 
     /**
