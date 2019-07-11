a6c8b145c81681164cf76021a58d92c6a119bd77
Stefan Lukas <afc11hn@hotmail.de>
2019-07-11 15:10:53 +0200

    Extract the history class into a new file

---
+++ src/main/java/de/dhbw/tinf18b4/chess/backend/History.java
@@ -0,0 +1,40 @@
+package de.dhbw.tinf18b4.chess.backend;
+
+import java.util.Stack;
+import java.util.stream.Stream;
+
+/**
+ * This class is used to store all the moves made by the player of a game.
+ * It allows adding and peeking operations which are backed by a stack internally.
+ * Additionally it can be used to obtain a complete listing of all the moves.
+ */
+public class History {
+    private Stack<Move> history = new Stack<>();
+
+    /**
+     * Add a move to history to make it the most recent move
+     *
+     * @param move the move
+     */
+    void addMove(Move move) {
+        history.add(move);
+    }
+
+    /**
+     * Return the most recent move without removing it from history
+     *
+     * @return the Move
+     */
+    public Move lastMove() {
+        return history.peek();
+    }
+
+    /**
+     * Returns all moves
+     *
+     * @return the moves
+     */
+    public Stream<Move> stream() {
+        return history.stream();
+    }
+}
