0ac16519fa552b11e081dfebd888d3b554447a91
Leonhard Gahr <leonhard.gahr@gmail.com>
2019-08-05 22:36:13 +0200

    anti xss again

--- src/main/java/de/dhbw/tinf18b4/chess/backend/utility/UserUtility.java
+++ src/main/java/de/dhbw/tinf18b4/chess/backend/utility/UserUtility.java
@@ -48,17 +48,6 @@ public class UserUtility {
      * @return the html-free string
      */
     private static String escapeHTML(String s) {
-        StringBuilder out = new StringBuilder(Math.max(16, s.length()));
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c > 127 || c == '"' || c == '<' || c == '>' || c == '&') {
-                out.append("&#");
-                out.append((int) c);
-                out.append(';');
-            } else {
-                out.append(c);
-            }
-        }
-        return out.toString();
+        return s.replaceAll("[<>]", "");
     }
 }
